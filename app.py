b45655dd6f1d9debef1ed686f8d503b1795a8e94
Jie-Yan094 <jarita0904@gmail.com>
2025-05-14 15:37:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-#
+import streamlit as st
+import ee
+from google.oauth2 import service_account
+import geemap.foliumap as geemap
+
+# 從 Streamlit Secrets 讀取 GEE 服務帳戶金鑰 JSON
+service_account_info = st.secrets["GEE_SERVICE_ACCOUNT"]
+
+# 使用 google-auth 進行 GEE 授權
+credentials = service_account.Credentials.from_service_account_info(
+    service_account_info,
+    scopes=["https://www.googleapis.com/auth/earthengine"]
+)
+
+# 初始化 GEE
+ee.Initialize(credentials)
+
+
+###############################################
+st.set_page_config(la
